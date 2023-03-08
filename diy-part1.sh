a3987ea9c41afa013cda8b3d4bb7859702d980c6
lxg20082008 <61457095+lxg20082008@users.noreply.github.com>
2023-03-08 21:32:42 +0300

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
 # Define My Package
 #git clone https://github.com/vernesong/OpenClash package/molun/luci-app-openclash
@@ -26,3 +26,7 @@ echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/molun/luci-app-jd-dailybonus
 #git clone https://github.com/sirpdboy/luci-theme-opentopd package/molun/luci-theme-opentopd
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/molun/luci-theme-infinityfreedom
+git clone https://github.com/Lienol/openwrt-package/tree/main/luci-app-guest-wifi package/molun/luci-app-guest-wifi
+git clone https://github.com/Lienol/openwrt-package/tree/main/luci-app-socat package/molun/luci-app-socat
+git clone https://github.com/kenzok8/small-package/tree/main/ddns-scripts package/molun/ddns-scripts
+git clone https://github.com/kenzok8/small-package/tree/main/luci-app-guest-wifi package/molun/luci-app-guest-wifi
